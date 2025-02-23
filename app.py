b1e35428a61cf3c8aa98bcb3147fcd3ddb0e2ec2
ikram <ikrammarbasy17@gmail.com>
2025-02-23 18:11:55 +0700

    Add Documentation

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import numpy as np
 
+#Membuat Array/Matriks
+#Membuat Matriks 3x3
 matriks = np.array([[1, 2, 3],
                     [0, 1, 4],
                     [5, 6, 0]])
 
+#Menghitung Invers
 invers = np.linalg.inv(matriks)
 
+#Cetak Hasil nya
 print("Matriks:")
 print(matriks)
 print("Invers Matriks:")
